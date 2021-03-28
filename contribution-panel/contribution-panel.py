19399c847ff4f0c42b4813ccb26afec7ae9cf825
sharvil-patankar <sharvil.patankar@bordrin.com>
2021-03-28 19:30:42 -0400

    MVP

--- contribution-panel/contribution-panel.py
+++ contribution-panel/contribution-panel.py
@@ -3,102 +3,85 @@
 from __future__ import print_function
 import base64
 import requests
-import json
+import yaml
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 from samplebase import SampleBase
+import threading
+import datetime
+from .timing import RepeatedTimer
 
 
 class CommitHeatmap(SampleBase):
     def __init__(self, *args, **kwargs):
         super(CommitHeatmap, self).__init__(*args, **kwargs)
         self.data = {}
-        self.read_config("sensitive.json")
-        self.read_config("config.json")
+        self.read_config("sensitive.yaml")
+        self.read_config("config.yaml")
+        self.color = self.data["start-color"]
+        self.heatmap_flag = False
 
     def read_config(self, filename):
         with open(filename) as f:
-            self.data.update(json.load(f))
+            self.data.update(yaml.load(f))
 
     def get_heatmap(self):
-        heatmap_dict = {
+        heatmap_dict =  {
                 'NONE':            0.0,
                 'FIRST_QUARTILE':  0.25,
                 'SECOND_QUARTILE': 0.5,
                 'THIRD_QUARTILE':  0.75,
                 'FOURTH_QUARTILE': 1.0
-                    }
+                        }
         url = 'https://api.github.com/graphql'
-        query = {'query': '{ viewer { login } user(login: "%s") { id contributionsCollection { contributionCalendar { totalContributions weeks { contributionDays { color contributionLevel date weekday } firstDay } } } } }' % self.data['subject-username'] }
+        query = {'query': '{ viewer { login } user(login: "%s") { id contributionsCollection { contributionCalendar { totalContributions weeks { contributionDays { color contributionLevel date weekday } firstDay } } } } }' % self.data['username'] }
         headers = {'Authorization': 'token %s' % self.data['api-token']}
-        req = requests.post(url=url, json=query, headers=headers)
-        json_object = req.json()
-        led_array = np.zeros([53, 7])
+        try:
+            req = requests.post(url=url, json=query, headers=headers)
+            json_object = req.json()
+            led_array = np.zeros([53, 7])
+        except Exception as ex:
+            print(ex)
 
         for r, weeks in enumerate(json_object['data']['user']['contributionsCollection']['contributionCalendar']['weeks']):
             for c, days in enumerate(weeks['contributionDays']):
                 led_array[r, c] = heatmap_dict[days['contributionLevel']]
-        self.heatmap = np.transpose(led_array)
+        heatmap = np.transpose(led_array)
+        self.generate_heatmap_image(heatmap)
 
-    def dark_mode_selector(self, background):
-        # TODO
-        light_colormap_dict = {
-                'green': ,
-                'red': ,
-                'white': ,
-                'blue': ,
-                'purple',
-                'orange'
-                              }
-        dark_colormap_dict = {
-                'green': ,
-                'red': ,
-                'white': ,
-                'blue': ,
-                'purple': ,
-                'orange':
-                             }
-        if background == "light":
-            self.colormap_dict = light_colomap_dict
-        elif background = "dark":
-            self.colormap_dict = dark_colormap_dict
-
-    def color_selector(self, color):
-        self.colormap = dark_colormap_dict[color]
-
-    def generate_heatmap_image(self):
-        #TODO
-        pass
+    def generate_heatmap_image(self, heatmap_array):
+        image = np.dstack((np.full_like(heatmap_array, self.color/6.0),np.full_like(values, 1),values))
+        image = matplotlib.colors.hsv_to_rgb(image)
+        image = Image.fromarray(np.uint8(image*255)).convert('RGB')
+        with threading.Lock():
+            self.heatmap_image = image
+            self.heatmap_flag = True
 
     def display_time(self):
         #TODO
         pass
 
-    def update_screen(self):
-        """
-        This should populate a canvas with the latest data
-        """
-        #TODO
-        pass
-
 
     def run(self):
+        self.get_heatmap()
+        self.heatmap_getter = RepeatedTimer(self.data["interval"], self.get_heatmap)
         canvas = self.matrix.CreateFrameCanvas()
 
-        self.get_heatmap()
+
+        canvas.clear()
+
         while True:
-            canvas.Clear()
             """
             Code goes here
             """
-            # canvas = self.matrix.SwapOnVSync(canvas)
-
+            if self.heatmap_flag:
+                canvas.SetImage(self.heatmap_image,self.rows-53, self.columns-7)
+                canvas = self.matrix.SwapOnVSync(canvas)
+                self.heatmap_flag= False
 
 if __name__ == "__main__":
     commit_heatmap = CommitHeatmap()
-    commit_heatmap.get_heatmap()
-    """
+
     if (not commit_heatmap.process()):
         commit_heatmap.print_help()
-    """
